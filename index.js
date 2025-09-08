83bb042667b6dc722faa186b689bcf2bf7e5a000
Satheesh-Baabu <satheeshbaabum@gmail.com>
2025-09-08 20:40:26 +0530

    sample testing

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/trainers", trainerRoutes);
 
 app.use("/api/orders", orderRoutes);
-app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes)
 
 //  Default Route
 app.get("/", (req, res) => {
